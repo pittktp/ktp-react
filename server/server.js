f26a7a9e81ea08d0d7e40e6364d8e70d530eb7b8
Z'vonty Flugence <zvontyflugence@gmail.com>
2020-10-27 14:16:32 -0400

    Changed CORS to allow any origin

--- server/server.js
+++ server/server.js
@@ -16,7 +16,13 @@ AWS.config.update({
 // Set Up Middleware
 app.use(helmet());
 app.use(express.json());
-app.use(cors({ origin: process.env.CORS_ORIGIN || 'http://localhost:3000'}));
+app.use(cors());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // Set up Controllers
 const authController = require('./controllers/authController');
