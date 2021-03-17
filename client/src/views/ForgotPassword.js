ee9e354fe2fcc47a4243d604f3fdfdccc04bff32
Z'vonty Flugence <zvontyflugence@gmail.com>
2021-03-16 21:43:01 -0400

    Implemented automated forgot password functionality

--- client/src/views/ForgotPassword.js
+++ client/src/views/ForgotPassword.js
@@ -18,11 +18,25 @@ const ForgotPassword = (props) => {
   const [password, setPassword] = useState('');
   const [confirm, setConfirm] = useState('');
   const [resetCode, setResetCode] = useState('');
+  const [showResetForm, setShowResetForm] = useState(false);
+
+  const submitForgotPass = e => {
+    e.preventDefault();
+
+    Api.forgotPassword({ email })
+      .then(data => {
+        if (data.success) {
+          props.showToast('success', data.message);
+          setShowResetForm(true);
+        }
+      })
+      .catch(err => props.showToast('error', err.response.data.err));
+  }
 
   const handleReset = e => {
     e.preventDefault();
 
-    Api.forgotPassword({ email, reset_code: resetCode, password }).then(data => {
+    Api.resetPassword({ email, reset_code: resetCode, password }).then(data => {
       if (data.success) {
         props.showToast('success', data.message);
         props.history.push('/login');
@@ -31,64 +45,71 @@ const ForgotPassword = (props) => {
     .catch(err => props.showToast('error', err.response.data.err));
   }
 
+  const cardContent = !showResetForm ? (
+    <Card.Body>
+      <p>Enter your account email and, if found, you will be sent an email with your recovery code.</p>
+      <Form onSubmit={submitForgotPass}>
+        <Form.Group controlId='email'>
+          <Form.Control
+            name='email'
+            type='email'
+            placeholder='Email'
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </Form.Group>
+        <center>
+          <Button variant='primary' type='submit'>Submit</Button>
+        </center>
+      </Form>
+    </Card.Body>
+  ) : (
+    <Card.Body>
+      <p>Enter the reset code sent to your email and enter a new password.</p>
+      <Form onSubmit={handleReset}>
+        <Form.Group controlId='resetCode'>
+          <Form.Control
+            name='resetCode'
+            type='text'
+            placeholder='Reset Code'
+            onChange={e => setResetCode(e.target.value)}
+            required
+          />
+        </Form.Group>
+        <Form.Group controlId='newPass'>
+          <Form.Control
+            name='newPass'
+            type='password'
+            placeholder='New Password'
+            onChange={e => setPassword(e.target.value)}
+            required
+          />
+        </Form.Group>
+        <Form.Group controlId='confirm'>
+          <Form.Control
+            name='confirmPass'
+            type='password'
+            placeholder='Confirm Password'
+            onChange={e => setConfirm(e.target.value)}
+            required
+          />
+        </Form.Group>
+        <center>
+          <Button variant='primary' type='submit'>Submit</Button>
+        </center>
+      </Form>
+    </Card.Body>
+  );
+
   return (
     <div id='forgot-password'>
       <Layout>
         <div id='ktp-content'>
           <Card style={{ width: '50%', margin: '0 auto' }}>
             <Card.Header>
-              Forgot Password
+              {`${ showResetForm ? 'Reset' : 'Forgot' } Password`}
             </Card.Header>
-            <Card.Body>
-              <p>Enter your account email and new password. Ask the current Tech Chair for a reset code</p>
-              <Form onSubmit={handleReset}>
-              `<Form.Group controlId='email'>
-                  <Form.Control
-                    name='email'
-                    type='email'
-                    placeholder='Email'
-                    onChange={(e) => setEmail(e.target.value)}
-                    required
-                  />
-                </Form.Group>
-                <Form.Group controlId='resetCode'>
-                  <Form.Control
-                    name='resetCode'
-                    type='text'
-                    placeholder='Reset Code'
-                    onChange={e => setResetCode(e.target.value)}
-                    required
-                  />
-                </Form.Group>
-                <Form.Group controlId='newPass'>
-                  <Form.Control
-                    name='newPass'
-                    type='password'
-                    placeholder='New Password'
-                    onChange={e => setPassword(e.target.value)}
-                    isValid={password && confirm && password === confirm}
-                    required
-                  />
-                </Form.Group>
-                <Form.Group controlId='confirm'>
-                  <Form.Control
-                    name='confirmPass'
-                    type='password'
-                    placeholder='Confirm Password'
-                    onChange={e => setConfirm(e.target.value)}
-                    isInvalid={password !== confirm}
-                    isValid={password && confirm && password === confirm}
-                    required
-                  />
-                  <Form.Control.Feedback type='invalid'>
-                    Your password fields do not match
-                  </Form.Control.Feedback>
-                </Form.Group>
-                <center>
-                  <Button variant='primary' type='submit'>Submit</Button>
-                </center>
-              </Form>
-            </Card.Body>
+            { cardContent }
           </Card>
         </div>
       </Layout>
